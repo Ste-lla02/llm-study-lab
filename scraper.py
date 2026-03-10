92e0453fd739735ed6387a9b8ebc7f7dc00dd8c5
stella <stella.debiase@gmail.com>
2026-03-10 11:58:38 +0100

    week3 - day1 first commit

--- scraper.py
+++ scraper.py
@@ -6,7 +6,6 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 }
 
-
 def fetch_website_contents(url):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
